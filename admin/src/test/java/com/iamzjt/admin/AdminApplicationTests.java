07fae7599f5edbb177b14acee479fc3f53b11e40
iamzjt-front-end <iamzjt@foxmail.com>
2023-10-15 18:40:30 +0800

    init: backstage code init

---
+++ admin/src/test/java/com/iamzjt/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iamzjt.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
