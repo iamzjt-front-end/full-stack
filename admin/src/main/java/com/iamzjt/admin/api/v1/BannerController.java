0901b056cc512643dc21199f9a7284cac705fefa
iamzjt-front-end <iamzjt@foxmail.com>
2023-10-17 13:14:19 +0800

    feat: 一些注解

--- admin/src/main/java/com/iamzjt/admin/api/v1/BannerController.java
+++ admin/src/main/java/com/iamzjt/admin/api/v1/BannerController.java
@@ -4,12 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@Controller
+//@ResponseBody
 public class BannerController {
   // RestFul API
   // 自描述性
   // host:port/v1/banner/test
 
   @GetMapping("v1/banner/test")
+  // @PostMapping
+  // @PutMapping
+  // @DeleteMapping
+  // @RequestMapping
   public String test() {
     return "hello, IamZJT!";
   }
