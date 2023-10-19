8e82cb5c3222c6fdb4b99b3a003a5fbf987bbd30
iamzjt-front-end <iamzjt@foxmail.com>
2023-10-19 13:13:58 +0800

    feat: 面向抽象编程

---
+++ lol/src/Main.java
@@ -0,0 +1,9 @@
+import test.A;
+
+public class Main {
+  public static void main(String[] args) {
+    A a = new A();
+    a.print();
+    // System.out.println("Hello World!");
+  }
+}
---
+++ lol/src/test/B.java
@@ -0,0 +1,7 @@
+package test;
+
+public class B {
+  public void print() {
+    System.out.println("This is Class B");
+  }
+}
