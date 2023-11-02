3ceaf3edf1a3a5864b8264086f2bf3c02237f30e
iamzjt-front-end <iamzjt@foxmail.com>
2023-11-02 11:49:54 +0800

    feat: ISkill implements

--- lol/src/Main.java
+++ lol/src/Main.java
@@ -1,26 +1,51 @@
-import awkward.hero.Darius;
-import awkward.hero.Diana;
-import awkward.hero.Irelia;
+import abstraction.ISkill;
+// import awkward.hero.Darius;
+// import awkward.hero.Diana;
+// import awkward.hero.Irelia;
+import abstraction.hero.Darius;
+import abstraction.hero.Diana;
+import abstraction.hero.Irelia;
 
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Enter a Hero's Name");
-    String heroName = Main.getPlayerInput();
+  // public static void main(String[] args) {
+  //   System.out.println("Enter a Hero's Name");
+  //   String heroName = Main.getPlayerInput();
+  //
+  //   switch (heroName) {
+  //     case "Diana" -> {
+  //       Diana diana = new Diana();
+  //       diana.r();
+  //     }
+  //     case "Irelia" -> {
+  //       Irelia irelia = new Irelia();
+  //       irelia.r();
+  //     }
+  //     case "Darius" -> {
+  //       Darius darius = new Darius();
+  //       darius.r();
+  //     }
+  //     default -> System.out.println("Hero not found");
+  //   }
+  // }
 
-    switch (heroName) {
+  public static void main(String[] args) {
+    String name = Main.getPlayerInput();
+    ISkill iSkill;
+
+    switch (name) {
       case "Diana" -> {
-        Diana diana = new Diana();
-        diana.r();
+        iSkill = new Diana();
+        iSkill.r();
       }
       case "Irelia" -> {
-        Irelia irelia = new Irelia();
-        irelia.r();
+        iSkill = new Irelia();
+        iSkill.r();
       }
       case "Darius" -> {
-        Darius darius = new Darius();
-        darius.r();
+        iSkill = new Darius();
+        iSkill.r();
       }
       default -> System.out.println("Hero not found");
     }
