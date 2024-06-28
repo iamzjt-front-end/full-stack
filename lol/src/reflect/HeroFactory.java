b378988c54731435230d183170fef3aaac29241d
iamzjt-front-end <iamzjt@foxmail.com>
2024-06-28 14:28:41 +0800

    feat: 重构

--- lol/src/reflect/HeroFactory.java
+++ lol/src/reflect/HeroFactory.java
@@ -6,7 +6,8 @@ public class HeroFactory {
     // 元类
     // 类 是对象的抽象，元类是对类的描述
     // 要输入完整的路径：reflect.hero.Diana
-    Class <?> cla = Class.forName(name);
+    String classStr = "reflect.hero." + name;
+    Class<?> cla = Class.forName(classStr);
     Object obj = cla.getDeclaredConstructor().newInstance();
 
     return (ISkill)obj;
